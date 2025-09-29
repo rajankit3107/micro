cb4f9b97c6c0b0dacac3f68aff840acc838983ae
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-30 02:28:26 +0530

    feat : optimized sidebar for mobile devices

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { UserButton } from "@clerk/clerk-react";
+import { OrganizationSwitcher, UserButton } from "@clerk/clerk-react";
 import { SearchInput } from "./search-input";
 
 export const NavBar = () => {
@@ -8,6 +8,30 @@ export const NavBar = () => {
     <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
         <SearchInput />
+      </div>
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                maxWidth: "376px",
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                border: "1px solid #E5E7EB",
+                justifyContent: "space-between",
+                backgroundColor: "white",
+              },
+            },
+          }}
+        />
       </div>
       <UserButton />
     </div>
