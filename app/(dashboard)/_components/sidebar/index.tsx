63507f95b6088ddfc4ed92608c25f74e963396fc
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-29 15:14:50 +0530

    feat : create organization with clerk

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,7 +1,9 @@
+import { NewButton } from "./new-button";
+
 export default function Sidebar() {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-600 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
-      Sidebar
+      <NewButton />
     </aside>
   );
 }
