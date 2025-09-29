e2cecf80cdf087b513a68a0b679b569fdadaf3be
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-29 15:47:36 +0530

    feat : added organization list feature

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,8 +1,10 @@
+import { List } from "./list";
 import { NewButton } from "./new-button";
 
 export default function Sidebar() {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-600 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
+      <List />
       <NewButton />
     </aside>
   );
