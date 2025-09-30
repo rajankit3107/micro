290474e6182d068a3483e4a8d8c1bde095f21c71
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-30 23:37:05 +0530

    feat: added board-list

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,26 @@
+"use client";
+interface BaordListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export const BoardList = ({ orgId, query }: BaordListProps) => {
+  const data = []; //TODO : change to API call
+
+  if (!data?.length && query.search) {
+    return <div>Try searching for somehting else</div>;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <div>No Favorites</div>;
+  }
+
+  if (!data?.length) {
+    return <div>No micros created yet</div>;
+  }
+
+  return <div></div>;
+};
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { BoardList } from "./_components/board-list";
 import { EmptyOrg } from "./_components/empty-org";
+import { useOrganization } from "@clerk/nextjs";
 
 const DashboardPage = () => {
+  const searchParams = useSearchParams();
+  const { organization } = useOrganization();
+
+  // Extract query params
+  const search = searchParams.get("search") || undefined;
+  const favorites = searchParams.get("favorites") || undefined;
+
+  const query = { search, favorites };
+
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
-      <EmptyOrg />
+      {/* {JSON.stringify(query)} */}
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={query} />
+      )}
     </div>
   );
 };
